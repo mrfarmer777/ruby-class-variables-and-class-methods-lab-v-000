135bd9693b502f87c1f37ca34e84297f3da0f1a8
Matt Farmer <mrfarmer777@gmail.com>
2018-05-29 21:07:33 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,3 @@
-require 'pry'
 
 class Song
   attr_accessor :name, :artist, :genre
@@ -12,17 +11,5 @@ class Song
     @artist=artist
     @genre=genre
     @@count+=1
-
-    binding.pry
-    if !@@artists.include?(@artist)   #Artist NOT in the list?
-      @@artists<<@artist             #add them to the array
-    end
-
-    if @@genres[@genre]==nil         #Are you missing the genre?
-      @@genres[@genre]=1             #Instantiate a new genre with a count of 1
-    else
-      @@genres[@genre]+=1            #Otherwise, add one to the genre count
-    end
-
   end
 end
