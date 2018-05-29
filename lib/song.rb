8855c8814d77d81d7aa555fcf30e79c036a5e595
Matt Farmer <mrfarmer777@gmail.com>
2018-05-29 20:02:43 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,5 @@
 class Song(name, artist, genre)
-  attr_reader :name, :artist, :genre
+  attr_accessor :name, :artist, :genre
 
   @@count=0
   @@artists=[]
@@ -9,14 +9,16 @@ class Song(name, artist, genre)
     @name=name
     @artist=artist
     @genre=genre
-
     @@count+=1
+    
     if !artists.contains?(artist)   #Artist NOT in the list?
       @@artists<<artist             #add them to the array
     end
+
     if @@genres[genre]==nil         #Are you missing the genre?
       @@genres[genre]=1             #Instantiate a new genre with a count of 1
     else
       @@genres[genre]+=1            #Otherwise, add one to the genre count
     end
   end
+end
