4a61f718c151e241658a8a96709e8f85fb3e4e52
Matt Farmer <mrfarmer777@gmail.com>
2018-05-29 20:57:12 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,12 @@
 
-class Song(name, artist, genre)
+class Song
   attr_accessor :name, :artist, :genre
 
   @@count=0
   @@artists=[]
   @@genres={}
 
-  def initialize
+  def initialize(name, artist, genre)
     @name=name
     @artist=artist
     @genre=genre
